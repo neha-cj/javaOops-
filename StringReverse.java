d1a5c02741f743242394e9b190922f8365d85bc8
neha-cj <119199146+neha-cj@users.noreply.github.com>
2023-10-30 11:00:21 +0530

    Update StringReverse.java
    lab

--- StringReverse.java
+++ StringReverse.java
@@ -1,21 +1,13 @@
-public class StringReverse {
-    public static void main(String[] args) {
-        String input = "Hello, World!"; // Change this to the string you want to reverse
-
-        String reversed = reverseString(input);
-
-        System.out.println("Original string: " + input);
-        System.out.println("Reversed string: " + reversed);
-    }
-
-    public static String reverseString(String str) {
-        int length = str.length();
-        StringBuilder reversed = new StringBuilder();
-
-        for (int i = length - 1; i >= 0; i--) {
-            reversed.append(str.charAt(i));
-        }
-
-        return reversed.toString();
-    }
-}
+import java.util.*;
+class StringReverse{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter string: ");
+		String s = scan.nextLine();
+		String s2 = "";
+		for(int i = s.length() - 1; i >= 0; i--){
+			s2 += s.charAt(i);
+			}
+		System.out.println(s2);
+		}	
+	}
