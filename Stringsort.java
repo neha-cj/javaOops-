20552f2add67b5fd163cd75b611f3e12e01bd581
neha-cj <119199146+neha-cj@users.noreply.github.com>
2023-10-30 11:10:07 +0530

    Update Stringsort.java
    lab

--- Stringsort.java
+++ Stringsort.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 public class Stringsort {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of strings: ");
-        int n = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline
+        int n = sc.nextInt();
+        sc.nextLine(); // Consume the newline
 
         String[] strings = new String[n];
 
         for (int i = 0; i < n; i++) {
             System.out.print("Enter string " + (i + 1) + ": ");
-            strings[i] = scanner.nextLine();
+            strings[i] = sc.nextLine();
         }
 
         // Sort the array of strings in alphabetical order
@@ -25,6 +25,6 @@ public class Stringsort {
             System.out.println(str);
         }
 
-        scanner.close();
+        sc.close();
     }
 }
