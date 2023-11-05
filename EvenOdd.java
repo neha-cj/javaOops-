a74d335343c3133151cb680fca4bdfda5e39aaa7
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-05 11:33:00 +0530

    Create EvenOdd.java
    check a number  is even or odd

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+class EvenOdd{
+  public static void main(String args[]){
+    int n=16;
+    if(n%2==0){
+      System.out.println("Even");
+    }
+    else{
+      System.out.println("Number is odd");
+    }
+  }
+}
