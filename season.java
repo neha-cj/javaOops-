e627c055935a457d30290ff57291b0895429e10d
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-05 13:04:43 +0530

    Create season.java
    display season corresponding to diff months

---
+++ season.java
@@ -0,0 +1,17 @@
+class season{
+  public static void main(String args[]){
+    int m=4;
+    string s;
+    if(m==12||m==1||m==2)
+      s="winter";
+    else if(m==3|||m==4||m==5)
+      s="spring";
+    else if(m==6|||m==7||m==8)
+      s="summer";
+    else if(m==6|||m==7||m==8)
+      s="autumn";
+    else
+      s="bogus value";
+    System.out.println("season is"+s);
+  }
+}
