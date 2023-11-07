8338a862887b67560ec346757dedcbc632b96f9a
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-07 19:57:40 +0530

    Create Fibonacci.java
    java program to find Fibonacci series upto n number using recursion

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FibonacciRecursionWithUserInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Fibonacci series up to " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
