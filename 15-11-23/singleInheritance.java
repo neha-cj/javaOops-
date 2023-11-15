f70e24f34f6faf0b6863a0be20bbe52445b54dcd
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-15 16:07:52 +0530

    Update singleInheritance.java

--- 15-11-23/singleInheritance.java
+++ 15-11-23/singleInheritance.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 class Employee {
     void display() {
         System.out.println("Name of the class is Employee.");
@@ -9,36 +7,23 @@ class Employee {
         System.out.println("Salary of employee is 10000");
     }
 }
-
 class Engineer extends Employee {
+    void display() {
+        super.display();
+       
+    }
+
     void calcSalary() {
-        super.calcSalary(); // Using super keyword to call from base class
+        super.calcSalary(); // Using super keyword to call display() from the base class
+        System.out.println("Name of the class is Engineer.");
         System.out.println("Salary of an engineer is 20000.");
     }
 }
-
-public class InheritanceExample {
+public class singleInheritance {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-
-        do {
-            System.out.println("\nEnter 1 to display Employee salary, 2 to display Engineer salary, or 0 to exit: ");
-            choice = scanner.nextInt();
-
-            if (choice == 1) {
-                Employee employee = new Employee();
-                employee.display();
-                employee.calcSalary();
-            } else if (choice == 2) {
-                Engineer engineer = new Engineer();
-                engineer.display();
-                engineer.calcSalary();
-            } else if (choice != 0) {
-                System.out.println("Invalid choice!");
-            }
-        } while (choice != 0);
-
-        scanner.close();
+       Engineer engineer = new Engineer();
+       engineer.display();
+       engineer.calcSalary();        
     }
 }
+
