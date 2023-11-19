813fd59329ea2491954ad796566a1a3d83d2ea2a
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-19 15:59:37 +0530

    Update AbstractPolymorphism.java

--- 15-11-23/AbstractPolymorphism.java
+++ 15-11-23/AbstractPolymorphism.java
@@ -1,25 +1,20 @@
-import java.util.Scanner;
-
 abstract class Shape {
     abstract void numberOfSides();
 }
 
 class Rectangle extends Shape {
-    @Override
     void numberOfSides() {
         System.out.println("Rectangle has 4 sides.");
     }
 }
 
 class Triangle extends Shape {
-    @Override
     void numberOfSides() {
         System.out.println("Triangle has 3 sides.");
     }
 }
 
 class Hexagon extends Shape {
-    @Override
     void numberOfSides() {
         System.out.println("Hexagon has 6 sides.");
     }
@@ -27,8 +22,6 @@ class Hexagon extends Shape {
 
 public class AbstractPolymorphismExample {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         // Creating objects of each shape class
         Shape rectangle = new Rectangle();
         Shape triangle = new Triangle();
@@ -39,7 +32,5 @@ public class AbstractPolymorphismExample {
         rectangle.numberOfSides();
         triangle.numberOfSides();
         hexagon.numberOfSides();
-
-        scanner.close();
     }
 }
