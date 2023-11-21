e4c0e192c2bd0457d26ff8728bc3d8a41177029d
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-21 22:21:41 +0530

    Create TestMyException.java
    create our own exception

---
+++ 20-11-23/TestMyException.java
@@ -0,0 +1,25 @@
+import java.lang.Exception;
+class MyException extends Exception{
+    MyException(String message){
+        super(message);
+    }
+}
+class Main{
+    public static void main(String args[]){
+        int x=5,y=1000;
+        try{
+            float z= (float) x/(float)y;
+            if(z<0.01){
+                throw new MyException("Number us too small");
+            }
+        }
+        catch(MyException e){
+            System.out.println("caught my exception");
+            System.out.println(e.getMessage());
+            
+        }
+        finally{
+            System.out.println("I am always here");
+        }
+    }
+}
