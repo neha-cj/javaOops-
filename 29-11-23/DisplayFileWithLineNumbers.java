68da947fb0b2e59b78d02a31728a428b6b70c32f
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-29 16:09:12 +0530

    Update DisplayFileWithLineNumbers.java

--- 29-11-23/DisplayFileWithLineNumbers.java
+++ 29-11-23/DisplayFileWithLineNumbers.java
@@ -1,28 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class DisplayFileWithLineNumbers {
-    public static void main(String[] args) {
-        // Specify the file path
-        String filePath = "example.txt";
-
-        // Display the file with line numbers
-        displayFileWithLineNumbers(filePath);
-    }
-
-    private static void displayFileWithLineNumbers(String filePath) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            System.out.println("Displaying file with line numbers:");
-
-            int lineNumber = 1;
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(lineNumber + ": " + line);
-                lineNumber++;
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading from the file: " + e.getMessage());
-        }
-    }
+public class DisplayFileWithLines{
+	public static void main(String[] args){
+		System.out.print("enter the  filepath to read from : ");
+		String filepath = new Scanner(System.in).nextLine();
+		
+		display(filepath);
+	}
+	public static void display(String filepath){
+		try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+			System.out.println("Displaying file with line numbers:");
+			int lineNo=1;
+			String line;
+			while((line =reader.readLine())!=null){
+				System.out.println(lineNo +":"+ line);
+				lineNo++;
+			}
+		}catch(IOException e){
+			System.err.println("Error reading frmo fil: "+ e.getMessage());
+		}
+	}
+	
 }
