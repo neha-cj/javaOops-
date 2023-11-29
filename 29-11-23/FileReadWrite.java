55383560517bbcb049c4655cd3692f202795d5e7
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-29 16:09:41 +0530

    Update FileReadWrite.java

--- 29-11-23/FileReadWrite.java
+++ 29-11-23/FileReadWrite.java
@@ -1,44 +1,43 @@
+import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class FileReadWrite {
-    public static void main(String[] args) {
-        // Specify the input and output file paths
-        String inputFile = "input.txt";
-        String outputFile = "output.txt";
-
-        // Reading from a file
-        readFile(inputFile);
-
-        // Writing to a file
-        writeFile(outputFile);
-    }
-
-    private static void readFile(String inputFile) {
-        try (FileInputStream inputStream = new FileInputStream(inputFile)) {
-            System.out.println("Reading from the file:");
-
-            int data;
-            while ((data = inputStream.read()) != -1) {
-                System.out.print((char) data);
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading from the file: " + e.getMessage());
-        }
-    }
-
-    private static void writeFile(String outputFile) {
-        try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-            // Writing some content to the file
-            String content = "Hello, this is a file handling example in Java.\n" +
-                             "It demonstrates how to read from a file and write to another file.\n" +
-                             "Enjoy learning Java!";
-
-            outputStream.write(content.getBytes());
-            System.out.println("\nFile has been written successfully.");
-        } catch (IOException e) {
-            System.err.println("Error writing to the file: " + e.getMessage());
-        }
-    }
+public class fileReadWrite{
+	public static void main(String args[]){
+		System.out.print("enter the input filepath: ");
+		String inputfile = new Scanner(System.in).nextLine();
+	
+		System.out.print("Enter the output file path: ");
+		String outputfile =new Scanner(System.in).nextLine();
+		
+		readFile(inputfile);
+		writefile(outputfile);
+		
+	}
+	public static void readFile(String inputfile){
+		try(FileInputStream istr = new FileInputStream(inputfile)){
+		System.out.println("Reading from the Input file: ");
+		int data;
+		while((data = istr.read())!=-1){
+			System.out.println((char)data);
+		}
+		
+		}catch(IOException e){
+			System.err.println("Error reading from the file: "+ e.getMessage());
+		}
+	}
+	public static void writefile(String outputfile){
+		try(FileOutputStream ostr= new FileOutputStream(outputfile)){
+			String content="Hello, this is a file handling example in java\n"+
+			"It demonstrates how to read from a file and write to another file.\n"+
+			"hello java!!";
+			
+			ostr.write(content.getBytes());
+			System.out.println("\nOutPut File has been written succesfully.");
+			
+		}catch(IOException e){
+			System.err.println("Error writing to the file: "+e.getMessage());
+		}
+	}
 }
