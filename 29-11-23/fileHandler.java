3835b47d117f45a8cf007a26a2657f47ab2da3ce
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-29 16:10:23 +0530

    Update fileHandler.java

--- 29-11-23/fileHandler.java
+++ 29-11-23/fileHandler.java
@@ -1,44 +1,44 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 
-public class FileHandlerExample {
-    public static void main(String[] args) {
-        // Specify the file path
-        String filePath = "example.txt";
 
-        // Writing to a file
-        writeToFile(filePath);
-
-        // Reading from a file
-        readFromFile(filePath);
-    }
-
-    private static void writeToFile(String filePath) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            // Write some content to the file
-            writer.write("Hello, this is a file handling example in Java.\n");
-            writer.write("It demonstrates how to write to a file and read from it.\n");
-            writer.write("Enjoy learning Java!");
-
-            System.out.println("File has been written successfully.");
-        } catch (IOException e) {
-            System.err.println("Error writing to the file: " + e.getMessage());
-        }
-    }
-
-    private static void readFromFile(String filePath) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            System.out.println("\nReading from the file:");
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading from the file: " + e.getMessage());
-        }
-    }
+public class filehandler{
+	
+	public static void writeToFile(String filepath){
+		try(BufferedReader r = new BufferedReader(new InputStreamReader(System.in))){
+			System.out.print("enter some text to write to file: ");
+			String userInput = r.readLine();
+		
+	
+			try(BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))){
+				writer.write(userInput);
+			 	System.out.println("\nfile has been written successfully");
+			
+			}catch(IOException e){
+				System.out.println("Error writing to the file: "+ e.getMessage());
+		
+			}
+		}catch(IOException e){
+			System.out.println("Error reading user input: "+e.getMessage());
+		}
+		
+	}
+	public static void readFromFile(String filepath){
+		try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+			System.out.println("\nReading from the file:");
+			
+			String line;
+			while((line=reader.readLine())!= null){
+				System.out.println(line);
+				
+			}
+		}catch(IOException e){
+			System.out.println("Error reading from the file: "+e.getMessage());
+		}
+	}
+	public static void main(String args[]){
+		String filepath="EXample.txt";
+		writeToFile(filepath);
+		readFromFile(filepath);
+		
+	}
 }
