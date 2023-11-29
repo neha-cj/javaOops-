ad47243f8fdebb46e3f9fe84747fd5f421adaca4
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-29 16:10:59 +0530

    Update nofcharlines.java

--- 29-11-23/nofcharlines.java
+++ 29-11-23/nofcharlines.java
@@ -2,42 +2,37 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.Scanner;
 
-public class FileStatistics {
-    public static void main(String[] args) {
-        // Specify the file path
-        String filePath = "example.txt";
-
-        // Display file statistics
-        displayFileStatistics(filePath);
-    }
-
-    private static void displayFileStatistics(String filePath) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            int charCount = 0;
-            int lineCount = 0;
-            int wordCount = 0;
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Count characters
-                charCount += line.length();
-
-                // Count lines
-                lineCount++;
-
-                // Count words using StringTokenizer
-                StringTokenizer tokenizer = new StringTokenizer(line);
-                wordCount += tokenizer.countTokens();
-            }
-
-            System.out.println("File Statistics for " + filePath + ":");
-            System.out.println("Number of characters: " + charCount);
-            System.out.println("Number of lines: " + lineCount);
-            System.out.println("Number of words: " + wordCount);
-
-        } catch (IOException e) {
-            System.err.println("Error reading from the file: " + e.getMessage());
-        }
-    }
+public class FileInfo{
+	public static void main(String[] args){
+		System.out.print("enter the  filepath to read from : ");
+		String filepath = new Scanner(System.in).nextLine();
+		
+		displayFile(filepath);
+	}
+	private static void displayFile(String filepath){
+		try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+			int chars=0;
+			int linecount=0;
+			int word=0;
+			
+			String line;
+			while((line = reader.readLine())!=null){
+				chars+=line.length();
+				linecount++;
+				StringTokenizer tokenizer = new StringTokenizer(line);
+				word += tokenizer.countTokens();
+				
+				
+			}
+			System.out.println("File Statistics for "+filepath+ ":");
+			System.out.println("number of characters: "+ chars);
+			System.out.println("number of lines: "+linecount);
+			System.out.println("number of words: "+word);
+			
+		}catch(IOException e){
+			System.err.println("Error reading from the file: "+ e.getMessage());
+		}
+	}
 }
